181cad088cb5aec5968574331512f5c956640979
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-08-11 05:37:39 +0530

    Latest

--- Main/js/script.js
+++ Main/js/script.js
@@ -12,3 +12,20 @@ setInterval(function() {
     .end()
     .appendTo('#slideshow');
 },  3000);
+
+$(function () {
+
+  $(window).scroll(function(){
+    if( $(window).scrollTop() > 100 ) {
+
+      $("nav").addClass("white-nav-top");
+      $(".navbar-brand img").attr("src", "img/logo/1.png")
+
+    } else {
+
+      $("nav").removeClass("white-nav-top");
+      $(".navbar-brand img").attr("src", "img/logo/2.png")
+
+    }
+  });
+})

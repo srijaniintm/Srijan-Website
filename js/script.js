ba11480dd910cf16178bd3343b35335fcfb1a8e7
nirmalnishant645 <nirmalnishant645@gmail.com>
2018-10-23 04:07:53 +0530

    javascript

--- js/script.js
+++ js/script.js
@@ -67,3 +67,18 @@ $(function () {
   });
 
 });
+
+$(document).ready(function(){
+
+  $("#thover").click(function(){
+		$(this).fadeOut();
+    $("#tpopup").fadeOut();
+	});
+
+
+  $("#tclose").click(function(){
+		$("#thover").fadeOut();
+    $("#tpopup").fadeOut();
+	});
+
+});
